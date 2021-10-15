b1200844dbab2c17efd3cd1822d6843fd965491c
James Reeve <james.reeve@ibm.com>
2021-10-15 14:38:03 -0400

    test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,15 +2,15 @@ describe('Works with a local minio instance and environment variable configurati
   beforeEach(() => {
     cy.openJupyterLab();
   });
-  it('Can render a file from Object Storage', () => {
-    cy.visit('/');
+  // it('Can render a file from Object Storage', () => {
+    // cy.visit('/');
 
-    cy.get('[title="Object Storage Browser"]').click();
-    cy.get('.jp-DirListing-item').dblclick();
-    cy.contains('hello world').should('not.exist');
-    cy.contains('test.md').dblclick();
-    cy.contains('hello world');
-  });
+    // cy.get('[title="Object Storage Browser"]').click();
+    // cy.get('.jp-DirListing-item').dblclick();
+    // cy.contains('hello world').should('not.exist');
+    // cy.contains('test.md').dblclick();
+    // cy.contains('hello world');
+  // });
   it('Object Storage Browser Tab exists', () => {
     cy.visit('/');
     cy.get('[title="Object Storage Browser"]');
